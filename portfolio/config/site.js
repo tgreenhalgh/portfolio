0008ae0f2dc3f93729e8e2ae3b1bb7bcba47e604
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-02-15 15:52:04 -0800

    starts project

---
+++ portfolio/config/site.js
@@ -0,0 +1,39 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Thomas Greenhalgh Portfolio', // Navigation and Site Title
+  siteTitleAlt: 'Thomas Greenhalgh Portfolio site', // Alternative Site title for SEO
+  siteTitleShort: 'tgreenhalgh', // short_name for manifest
+  siteHeadline: 'Publishing & Creating stunning photos', // Headline for schema.org JSONLD
+  siteUrl: 'https://thomasgreenhalgh.cmo', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/logos/logo.png', // Used for SEO and manifest
+  siteDescription:
+    'Dark One-Page portfolio with cards & detailed project views',
+  author: 'tgreenhalgh', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@likothecat', // Twitter Username
+  ogSiteName: '', // Facebook Site Name
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-47519312-4',
+
+  // Manifest and Progress color
+  themeColor: '#3498DB',
+  backgroundColor: '#2b2e3c',
+
+  // Your information
+  avatar: '/logos/lekoarts.png',
+  name: 'LekoArts',
+  location: 'Germany',
+  socialMedia: [
+    {
+      url: 'https://www.github.com/tgreenhalgh',
+      name: 'Github',
+    },
+    {
+      url: 'https://thomasgreenhalgh',
+      name: 'Homepage',
+    },
+  ],
+};
---
+++ portfolio/gatsby-node.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const _ = require('lodash')
+
+const wrapper = promise => promise.then(result => ({ result, error: null })).catch(error => ({ error, result: null }))
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+  let slug
+  // Search for MDX filenodes
+  if (node.internal.type === 'Mdx') {
+    // If the frontmatter has a "slug", use it
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'slug')
+    ) {
+      slug = `/${_.kebabCase(node.frontmatter.slug)}`
+    }
+    // If not derive a slug from the "title" in the frontmatter
+    if (
+      Object.prototype.hasOwnProperty.call(node, 'frontmatter') &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, 'title')
+    ) {
+      slug = `/${_.kebabCase(node.frontmatter.title)}`
+    }
+    createNodeField({ node, name: 'slug', value: slug })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const projectTemplate = require.resolve('./src/templates/project.js')
+
+  const { error, result } = await wrapper(
+    graphql(`
+      {
+        projects: allMdx {
+          edges {
+            node {
+              fileAbsolutePath
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `)
+  )
+
+  if (!error) {
+    const projectPosts = result.data.projects.edges
+
+    projectPosts.forEach((edge, index) => {
+      const next = index === 0 ? null : projectPosts[index - 1].node
+      const prev = index === projectPosts.length - 1 ? null : projectPosts[index + 1].node
+
+      createPage({
+        path: edge.node.fields.slug,
+        component: projectTemplate,
+        context: {
+          slug: edge.node.fields.slug,
+          // Pass the current directory of the project as regex in context so that the GraphQL query can filter by it
+          absolutePathRegex: `/^${path.dirname(edge.node.fileAbsolutePath)}/`,
+          prev,
+          next,
+        },
+      })
+    })
+    return
+  }
+
+  console.log(error)
+}
