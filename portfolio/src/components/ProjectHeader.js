83d6fb76f53b1e1de35bb48c94138df87fe7606a
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2019-02-23 11:52:17 -0800

    removes date from page view

--- portfolio/src/components/ProjectHeader.js
+++ portfolio/src/components/ProjectHeader.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import { useSpring, animated, config } from 'react-spring'
-import MDXRenderer from 'gatsby-mdx/mdx-renderer'
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { useSpring, animated, config } from 'react-spring';
+import MDXRenderer from 'gatsby-mdx/mdx-renderer';
 
-import arrow from '../images/left-chevron.svg'
+import arrow from '../images/left-chevron.svg';
 
 const Wrapper = styled.div`
   display: flex;
   position: relative;
-`
+`;
 
 const Content = styled.div`
   margin: 0 auto;
@@ -18,7 +18,7 @@ const Content = styled.div`
   max-width: ${props => props.theme.maxWidths.general};
   padding: 2rem 1.0875rem 13rem 1.0875rem;
   color: ${props => props.theme.colors.secondary};
-`
+`;
 
 const Back = styled(Link)`
   display: flex;
@@ -31,7 +31,7 @@ const Back = styled(Link)`
     height: 1.5rem;
     margin: 0 1rem 0 0;
   }
-`
+`;
 
 const Avatar = styled.div`
   height: 3rem;
@@ -46,12 +46,12 @@ const Avatar = styled.div`
     height: auto;
     width: 100%;
   }
-`
+`;
 
 const Name = styled(animated.h4)`
   margin: 0 0 0 1rem;
   color: ${props => props.theme.colors.color};
-`
+`;
 
 const Details = styled.div`
   width: 100%;
@@ -61,13 +61,13 @@ const Details = styled.div`
   h1 {
     color: ${props => props.theme.colors.color};
   }
-`
+`;
 
 const Text = styled.div`
   max-width: 750px;
   margin: 4rem auto 2rem auto;
   color: white;
-`
+`;
 
 const ProjectHeader = ({ avatar, name, title, date, areas, text }) => {
   const titleProps = useSpring({
@@ -75,14 +75,24 @@ const ProjectHeader = ({ avatar, name, title, date, areas, text }) => {
     delay: 200,
     from: { opacity: 0, transform: 'translate3d(0, 30px, 0)' },
     to: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
-  })
-  const contentProps = useSpring({ config: config.slow, delay: 600, from: { opacity: 0 }, to: { opacity: 1 } })
+  });
+  const contentProps = useSpring({
+    config: config.slow,
+    delay: 600,
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  });
 
   return (
     <Wrapper>
       <Content>
         <Back to="/">
-          <img src={arrow} data-info="back" alt="Back to home" aria-label="Back to home" />
+          <img
+            src={arrow}
+            data-info="back"
+            alt="Back to home"
+            aria-label="Back to home"
+          />
           <Avatar>
             <img src={avatar} alt={name} />
           </Avatar>
@@ -91,7 +101,7 @@ const ProjectHeader = ({ avatar, name, title, date, areas, text }) => {
         <Details>
           <animated.h1 style={titleProps}>{title}</animated.h1>
           <animated.div style={contentProps}>
-            <p>{date}</p>
+            {/* <p>{date}</p> */}
             <div>
               {areas.map((area, index) => (
                 <React.Fragment key={area}>
@@ -109,10 +119,10 @@ const ProjectHeader = ({ avatar, name, title, date, areas, text }) => {
         </Details>
       </Content>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default ProjectHeader
+export default ProjectHeader;
 
 ProjectHeader.propTypes = {
   avatar: PropTypes.string.isRequired,
@@ -121,4 +131,4 @@ ProjectHeader.propTypes = {
   date: PropTypes.string.isRequired,
   areas: PropTypes.array.isRequired,
   text: PropTypes.string.isRequired,
-}
+};
